221470e43f1cd609d1c5c2d630e6ee8c2c11901d
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-06-11 11:42:50 -0300

    Update controllers auth,add schemas with JOI

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,10 +1,10 @@
 import User from "../../models/User.js";
 
 const signout = async (req, res, next) => {
-  const { email } = req.params;
+  const { email } = req.body;
   try {
     await User.findOneAndUpdate({ email }, { is_online: false });
-    return res.status(200).send({ message: "User logged out" });
+    return res.status(200).json({ message: "User logged out" });
   } catch (error) {
     next(error);
   }
